5af476b39262060a6259610ce95f4dd888203aac
Qamar Usman <132997659+REASCREH@users.noreply.github.com>
2025-06-08 03:54:24 -0700

    Update app01.py

--- app01.py
+++ app01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import xgboost as xgb
+#import xgboost as xgb
 from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
